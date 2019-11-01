ad1d2178900b5327f0f36e550ce8d39a7230ea0e
fangxing <450499752@qq.com>
2019-11-01 18:07:52 +0800

    。。。

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,33 @@
 <template>
-	<view>
+	<view class="content">
 		<view class="content-picker">
 			<view class="picker picker-school">
-				<text>学校</text>
+				<view>学校</view>
 			</view>
 			<view class="picker picker-course">
-				<utext>班课类型</utext>
+				<view>班课类型</view>
+			</view>
+		</view>
+		<view class="content-list" v-for="item in AllData" :key="item.classId">
+			<view class="list">
+				<image class="list-img" src="https://dueape-nation-1255328906.cos.ap-singapore.myqcloud.com/image/2019103006105513.jpg"></image>
+				<view class="list-main">
+					<view class="list-title">{{ item.title }}</view>
+					<view class="list-main-content">
+						<text>线上</text>
+						<image class="list-main-img list-right" src="/static/time.png"></image>
+						<text>{{ item.updateTime }}</text>
+					</view>
+					<view class="list-main-content">
+						<image class="list-main-img" src="/static/bm.png"></image>
+						<text class="text-color">{{ item.applyNum }}</text>
+						<text>人报名</text>
+					</view>
+					<view class="list-main-content">
+						<text class="text-color">$</text>
+						<text class="text-color">{{ item.coursePrice }}</text>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -14,9 +36,28 @@
 <script>
 	export default {
 		data() {
-			return {}
+			return {
+				AllData: []
+			}
 		},
 		onLoad() {
+			uni.request({
+			    url: 'https://weixin.dueape.com/dueape/class/index',
+				method: 'POST',
+				data: {
+					order: 'desc',
+					sortField: 'class_id',
+					pageSize: 10,
+					pageNum: 1,
+					displayFlag: 1,
+					courseType: undefined,
+					collegeId: undefined
+				},
+			    success: (res) => {
+					const data = res.data
+					this.AllData = data.results.data
+			    }
+			})
 		},
 		methods: {
 		}
@@ -29,7 +70,10 @@
 	}
 	.content-picker {
 		height: 70rpx;
-		padding: 15rpx 25rpx;
+		padding-top: 15rpx;
+		padding-right: 25rpx;
+		padding-bottom: 15rpx;
+		padding-left: 25rpx;
 		background: #fff;
 		display: flex;
 		flex-direction: row;
@@ -56,5 +100,68 @@
 		background: #e8e8e8;
 		display: inline-flex;
 		align-items: center;
+	}
+	.content-list {
+		margin-top: 30rpx;
+		margin-right: 25rpx;
+		margin-left: 25rpx;
+		padding-top: 20rpx;
+		padding-right: 30rpx;
+		padding-bottom: 20rpx;
+		padding-left: 30rpx;
+		border-radius: 10rpx;
+		box-shadow: 0rpx -2rpx 8rpx rgba(0, 0, 0, .06);
+		background: #fff;
+	}
+	.list {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.list-img {
+		width: 216rpx;
+		height: 172rpx;
+		will-change: transform;
+	}
+	.list-main {
+		flex: 1;
+		display: inline-flex;
+		flex-direction: column;
+		justify-content: space-between;
+		margin-left: 24rpx;
+	}
+	.list-main-content {
+		width: 100%;
+		color: #999;
+		font-size: 26rpx;
+		line-height: 28rpx;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	.list-title {
+		color: #333;
+		font-size: 28rpx;
+		font-weight: bold;
+		width: 100%;
+		line-height: 30rpx;
+	}
+	.list-main-img {
+		width: 26rpx;
+		height: 26rpx;
+		margin-right: 15rpx;
+	}
+	.list-right {
+		margin-left: 15rpx;
+	}
+	.text-color {
+		color: #ff9704;
+		font-size: 32rpx;
+		line-height: 34rpx;
+		margin-right: 10rpx;
+	}
+	.text-weight {
+		font-weight: bold;
 	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -131,7 +131,29 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -147,11 +169,31 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 var _default =
 {
   data: function data() {
-    return {};
+    return {
+      AllData: [] };
+
   },
-  onLoad: function onLoad() {
+  onLoad: function onLoad() {var _this = this;
+    uni.request({
+      url: 'https://weixin.dueape.com/dueape/class/index',
+      method: 'POST',
+      data: {
+        order: 'desc',
+        sortField: 'class_id',
+        pageSize: 10,
+        pageNum: 1,
+        displayFlag: 1,
+        courseType: undefined,
+        collegeId: undefined },
+
+      success: function success(res) {
+        var data = res.data;
+        _this.AllData = data.results.data;
+      } });
+
   },
   methods: {} };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ }),
 /* 21 */
